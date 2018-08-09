4fff172c7aca92f681a23d971bca18fd31d46db3
Jesus Chavez <jesus.s.chavez01@gmail.com>
2018-08-09 19:54:03 -0400

    project setup

---
+++ desktop/src/com/zordo/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zordo.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zordo.game.LegendOfZordo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LegendOfZordo(), config);
+	}
+}
