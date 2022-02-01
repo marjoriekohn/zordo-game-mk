43ef87c42e70c77a2805474fe486d970b9753f24
jesusch4vez <jesus.s.chavez01@gmail.com>
2022-02-01 14:06:08 -0500

    cleaning up assets and animation

--- core/src/com/zordo/screens/PlayScreen.java
+++ core/src/com/zordo/screens/PlayScreen.java
@@ -1,34 +1,39 @@
 package com.zordo.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.zordo.game.LegendOfZordo;
 
 public class PlayScreen implements Screen{
 	
 	Rectangle player;
-	private Sprite playerTexture;
+
 	private SpriteBatch batch;
-	private TextureAtlas atlas;
 	private TextureRegion background;
 	private Texture backgroundTexture;
 	
-	private Boolean flippedLeft;
 	private Boolean flippedRight;
 	
 	OrthographicCamera camera;
+	
+	Animation<Sprite> animation;
+	Animation<Sprite> runningRightAnimation;
+	Animation<Sprite> runningLeftAnimation;
+	
+	Sprite[] runningRightFrames;
+	Sprite[] runningLeftFrames;
+	Sprite standRight;
+	Sprite standLeft;
+	
+	float elapsed;
 	
 	final LegendOfZordo game;
 
@@ -38,11 +43,28 @@ public class PlayScreen implements Screen{
 		player.x = 10;
 		player.y = 10;
 		
-		flippedLeft = false;
 		flippedRight = true;
 		
-		atlas = new TextureAtlas("link-sprites.txt");
-		playerTexture = new Sprite(atlas.createSprite("link-standing-0"));
+		runningRightFrames = new Sprite[6];
+		runningLeftFrames = new Sprite[6];
+		
+		String standuri = "link-standing-sprites/";
+		String runuri = "link-running-sprites/";
+		
+		standRight = new Sprite(new Texture(standuri + "link-standing-0.png"));
+		standLeft = new Sprite(new Texture(standuri + "link-standing-0.png"));
+		standLeft.flip(true, false);
+		
+		for(int i = 0; i < 6; i++) {
+			runningRightFrames[i] = new Sprite(new Texture(runuri + "link-running-" + i + ".png"));
+			runningLeftFrames[i] = new Sprite(new Texture(runuri + "link-running-" + i + ".png"));
+			runningLeftFrames[i].flip(true, false);
+		}
+
+		runningRightAnimation = new Animation<Sprite>(1f/4f, runningRightFrames);
+		runningLeftAnimation = new Animation<Sprite>(1f/4f, runningLeftFrames);
+		animation = runningRightAnimation;
+		
 		background = new TextureRegion();
 		backgroundTexture = new Texture("background_32.png");
 		background.setTexture(backgroundTexture);
@@ -55,44 +77,51 @@ public class PlayScreen implements Screen{
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		System.out.print("hello, welcome to playscreen");
 		
 	}
 
 	@Override
 	public void render(float delta) {
-		ScreenUtils.clear(Color.BLACK);
-		
 		batch = new SpriteBatch();
 		batch.setProjectionMatrix(camera.combined);
 		
 		batch.begin();
 		batch.draw(backgroundTexture,0,0,800,480);
-		batch.draw(playerTexture, player.x,player.y);
 
+
+		if(!Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			elapsed += Gdx.graphics.getDeltaTime();
+			if(flippedRight) {
+				batch.draw(standRight, player.x, player.y);
+			} else {
+				batch.draw(standLeft, player.x, player.y);
+			}
+		} else { 
+			elapsed += Gdx.graphics.getDeltaTime();
+			if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+				if(!flippedRight) {
+					flippedRight = true;
+				}
+				animation = runningRightAnimation;
+				player.x += 200 * Gdx.graphics.getDeltaTime();
+			} else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				if(flippedRight) {
+					flippedRight = false;
+				}
+				animation = runningLeftAnimation;
+				player.x -= 200 * Gdx.graphics.getDeltaTime();
+			} 
+			batch.draw(animation.getKeyFrame(elapsed,true), player.x,player.y);
+		}
 		
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			if(!flippedRight) {
-				playerTexture.flip(true, false);
-				flippedRight = true;
-				flippedLeft = false;
-			}
-			player.x += 500 * Gdx.graphics.getDeltaTime();
-		} else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			if(!flippedLeft) {
-				playerTexture.flip(true, false);
-				flippedRight = false;
-				flippedLeft = true;
-			}
-			player.x -= 500 * Gdx.graphics.getDeltaTime();
-		} else if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
 			player.y += 1000 * Gdx.graphics.getDeltaTime();
 		}
 		
 		if(player.y > 10) {
 			player.y -= 25 * Gdx.graphics.getDeltaTime();
 		}
-
+		
 		batch.end();
 		camera.update();
 	}
