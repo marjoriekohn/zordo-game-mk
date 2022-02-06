8e1934f55a50164ac8d8065d9a1ecf0c1ab8beb3
jesusch4vez <jesus.s.chavez01@gmail.com>
2022-02-06 17:36:30 -0500

    refactored player rendering logic and added some health info, refactored animation setting

--- core/src/com/zordo/character/Linko.java
+++ core/src/com/zordo/character/Linko.java
@@ -1,45 +1,78 @@
 package com.zordo.character;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
-public class Linko {
+public class Linko {	
 	Rectangle linkoCollider;
 	Boolean flippedRight;
 	Boolean jumping;
+	Boolean playerControlled;
+	Boolean stepping;
+	
+	int health;
+	Sprite[] hearts;
 		
 	Animation<Sprite> runningRightAnimation;
 	Animation<Sprite> runningLeftAnimation;
+	Animation<Sprite> walkingRightAnimation;
+	Animation<Sprite> walkingLeftAnimation;
+	Animation<Sprite> animation;
 	
 	Sprite[] runningRightFrames;
 	Sprite[] runningLeftFrames;
+	Sprite[] walkingRightFrames;
+	Sprite[] walkingLeftFrames;
 	Sprite standRight;
 	Sprite standLeft;
 	Sprite jumpRight;
 	Sprite jumpLeft;
+	
+	int jumps = 0;
+	float jump2 = 0;
+	
+	float elapsed;
+	float elapsed2;
 
 	// accessing sprite assets from the directory files
 	String standuri = "link-standing-sprites/";
 	String runuri = "link-running-sprites/";
 	String jumpuri = "link-jumping-sprites/";
+	String heartsuri = "hearts/";
 		
-	public Linko() {
+	public Linko(Boolean playerControlled) {
+		this.playerControlled = playerControlled;
 		linkoCollider = new Rectangle();
 		linkoCollider.x = 10;
 		linkoCollider.y = 10;
 		linkoCollider.height = 25;
 		linkoCollider.width = 15;
 		
+		health = 4;
+		hearts = new Sprite[6];
+		
+		
+		// heart textures
+		for(int i = 0; i < 5; i++) {
+			hearts[i] = new Sprite(new Texture(heartsuri + "heart-" + i + ".png"));
+			hearts[i].scale(10);
+		}
+		
 		// orientation and jumping status
 		flippedRight = true;
 		jumping = false;
+		stepping = false;
 		
 		// animation objects
 		runningRightFrames = new Sprite[6];
 		runningLeftFrames = new Sprite[6];
-		
+		walkingRightFrames = new Sprite[4];
+		walkingLeftFrames = new Sprite[4];
 		
 		// standing and jumping sprite creation
 		standRight = new Sprite(new Texture(standuri + "link-standing-0.png"));
@@ -50,19 +83,141 @@ public class Linko {
 		jumpLeft.flip(true, false);
 		
 		// running animation construction
+		int j = 0;
 		for(int i = 0; i < 6; i++) {
 			runningRightFrames[i] = new Sprite(new Texture(runuri + "link-running-" + i + ".png"));
 			runningLeftFrames[i] = new Sprite(new Texture(runuri + "link-running-" + i + ".png"));
+			if( i != 2 && i != 5) {
+				System.out.println("adding walk frame: " + j);
+				walkingRightFrames[j] = new Sprite(new Texture(runuri + "link-running-" + i + ".png"));
+				walkingLeftFrames[j] = new Sprite(new Texture(runuri + "link-running-" + i + ".png"));
+				walkingLeftFrames[j].flip(true, false);
+				j++;
+			}
 			runningLeftFrames[i].flip(true, false);
 		}
 
 		// setting the animations
 		runningRightAnimation = new Animation<Sprite>(1f/4f, runningRightFrames);
 		runningLeftAnimation = new Animation<Sprite>(1f/4f, runningLeftFrames);
+		walkingRightAnimation = new Animation<Sprite>(1f/4f, walkingRightFrames);
+		walkingLeftAnimation = new Animation<Sprite>(1f/4f, walkingLeftFrames);
+		
+		animation = walkingRightAnimation;
+	}
+	
+	public void move(SpriteBatch batch) {
+		elapsed += Gdx.graphics.getDeltaTime();
+		stepping = true;
+
+		if(playerControlled) {
+			if(!Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+				stepping = false;
+				if(this.getFlippedRight() && !this.getJumping()) {
+					this.setStandRight(this.getStandRight());
+					batch.draw(this.getStandRight(), this.getLinkoCollider().x, this.getLinkoCollider().y);
+				} else if (!this.getJumping()) {
+					batch.draw(this.getStandLeft(), this.getLinkoCollider().x, this.getLinkoCollider().y);
+				}
+			} 
+			if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+				if(!this.getFlippedRight()) {
+					this.setFlippedRight(true);;
+				}
+				if(!this.getJumping()){
+					animation = this.getWalkingRightAnimation();
+				}
+				if(!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
+					this.getLinkoCollider().x += 100 * Gdx.graphics.getDeltaTime();
+				} else {
+					animation = this.getRunningRightAnimation();
+					this.getLinkoCollider().x += 150 * Gdx.graphics.getDeltaTime();
+				}
+			} else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				if(this.getFlippedRight()) {
+					this.setFlippedRight(false);
+				}
+				if(!this.getJumping()){
+					animation = this.getRunningLeftAnimation();
+				}
+				if(!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
+					this.getLinkoCollider().x -= 100 * Gdx.graphics.getDeltaTime();
+				} else {
+					this.getLinkoCollider().x -= 150 * Gdx.graphics.getDeltaTime();
+				}
+			} 
+			
+			if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && jumps < 2) {
+				this.setJumping(true);
+				if(jumps == 0) {
+					this.getLinkoCollider().y += (100 * Gdx.graphics.getDeltaTime()) + 75;
+					if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
+						this.getLinkoCollider().y += 50;
+					}
+					jump2 = this.getLinkoCollider().y * 1.5f;
+					System.out.println("jump 1: " + this.getLinkoCollider().y);
+				} else {
+					System.out.println("jump 2 start: " + this.getLinkoCollider().y);
+					this.getLinkoCollider().y = jump2;
+					System.out.println("jump 2: " + this.getLinkoCollider().y);
+				}
+				jumps++;
+			} 
+			
+			if(this.getJumping()) {
+				stepping = false;
+				jumpRender(batch);
+			}
+			
+			if(this.getLinkoCollider().y > 11) {
+				this.getLinkoCollider().y -= 120 * Gdx.graphics.getDeltaTime();
+			} else if (this.getLinkoCollider().y < 11) {
+				jumps = 0;
+				this.setJumping(false);
+			}
+		} else {
+			if(!this.getJumping()){
+				animation = this.getRunningLeftAnimation();
+			}
+				this.getLinkoCollider().x -= 100 * Gdx.graphics.getDeltaTime();
+		}
+		if(stepping) {
+			batch.draw(animation.getKeyFrame(elapsed,true), this.getLinkoCollider().x,this.getLinkoCollider().y);
+		}
+	}
+	
+	private void jumpRender(SpriteBatch batch) {
+		if(this.getFlippedRight()) {
+			batch.draw(this.getJumpRight(), this.getLinkoCollider().x, this.getLinkoCollider().y);
+		} else {
+			batch.draw(this.getJumpLeft(), this.getLinkoCollider().x, this.getLinkoCollider().y);
+		}
+	}
+	
+	public void healthRender(SpriteBatch batch) {
+		switch (this.health) {
+			case 4: batch.draw(hearts[4], 10, 250);
+				break;
+			case 3: batch.draw(hearts[3], 10,250);
+				break;
+			case 2: batch.draw(hearts[2], 10,250);
+				break;
+			case 1: batch.draw(hearts[1], 10,250);
+				break;
+			case 0: batch.draw(hearts[0], 10,250);
+				break;
+			default: batch.draw(hearts[0], 10,250);
+				break;
+		}
 	}
 	
 	public void setLinkoCollider(Rectangle linkoCollider) {
 		this.linkoCollider = linkoCollider;
+	}
+	
+	public void setLinkoCollider(float x, float y) {
+		this.linkoCollider.x = x;
+		this.linkoCollider.y = y;
 	}
 	
 	public Rectangle getLinkoCollider() {
@@ -84,6 +239,22 @@ public class Linko {
 	public Boolean getJumping() {
 		return this.jumping;
 	};
+	
+	public void setWalkingRightAnimation(Animation<Sprite> walkingRightAnimation) {
+		this.walkingRightAnimation = walkingRightAnimation;
+	}
+	
+	public Animation<Sprite> getWalkingRightAnimation () {
+		return this.walkingRightAnimation;
+	}
+	
+	public void setWalkingLeftAnimation(Animation<Sprite> walkingLeftAnimation) {
+		this.walkingLeftAnimation = walkingLeftAnimation;
+	}
+	
+	public Animation<Sprite> getWalkingLeftAnimation () {
+		return this.walkingLeftAnimation;
+	}
 	
 	public void setRunningRightAnimation(Animation<Sprite> runningRightAnimation) {
 		this.runningRightAnimation = runningRightAnimation;
@@ -149,5 +320,11 @@ public class Linko {
 		return this.jumpLeft;
 	};
 	
+	public void setHealth(int health) {
+		this.health = health;
+	}
 	
+	public int getHealth() {
+		return this.health;
+	}
 }
--- core/src/com/zordo/screens/PlayScreen.java
+++ core/src/com/zordo/screens/PlayScreen.java
@@ -1,47 +1,28 @@
 package com.zordo.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
 import com.zordo.character.Linko;
 import com.zordo.game.LegendOfZordo;
 
 public class PlayScreen implements Screen{
-	
-	Rectangle other;
 
 	private SpriteBatch batch;
 	private TextureRegion background;
 	private Texture backgroundTexture;
 	
 	OrthographicCamera camera;
-
-	float elapsed;
-	float elapsed2;
 	
 	final LegendOfZordo game;
 	Linko linko;
-	Linko otherLinko;
+	Linko linko2;
+	Linko linko3;
 	
-	int jumps;
-	float jump2;
-
 	public PlayScreen(final LegendOfZordo game) {
 		this.game = game;
-
-		other = new Rectangle();
-		other.x = 300;
-		other.y = 10;
-		other.width = 10;
-		other.height = 10;
 
 		// setting the background texture
 		background = new TextureRegion();
@@ -52,16 +33,12 @@ public class PlayScreen implements Screen{
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800,400);
 		
-		linko = new Linko();
-		otherLinko = new Linko();	
-		
-		elapsed = 0;
-		elapsed2 = 13.50003323654f;
-		
-		jumps = 0;
-		jump2 = 0;
-		
-		otherLinko.setRunningLeftAnimation(new Animation<Sprite>(1f/6f,otherLinko.getRunningLeftFrames()));
+		linko = new Linko(true);
+		linko2 = new Linko(false);
+		linko3 = new Linko(false);
+	
+		linko2.setLinkoCollider(400, 10);
+		linko3.setLinkoCollider(400,100);
 	}
 	
 	@Override
@@ -77,89 +54,25 @@ public class PlayScreen implements Screen{
 		
 		batch.begin();
 		batch.draw(backgroundTexture,0,0,800,400);
-		elapsed += Gdx.graphics.getDeltaTime();		
-		elapsed2 += Gdx.graphics.getDeltaTime();
-
-		if(!Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			if(linko.getFlippedRight() && !linko.getJumping()) {
-				linko.setStandRight(linko.getStandRight());
-				batch.draw(linko.getStandRight(), linko.getLinkoCollider().x, linko.getLinkoCollider().y);
-			} else if (!linko.getJumping()) {
-				batch.draw(linko.getStandLeft(), linko.getLinkoCollider().x, linko.getLinkoCollider().y);
-			}
-		} 
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			if(!linko.getFlippedRight()) {
-				linko.setFlippedRight(true);;
-			}
-			if(!linko.getJumping()){
-				batch.draw(linko.getRunningRightAnimation().getKeyFrame(elapsed,true), linko.getLinkoCollider().x,linko.getLinkoCollider().y);
-			}
-			if(!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-				linko.getLinkoCollider().x += 100 * Gdx.graphics.getDeltaTime();
-			} else {
-				linko.getLinkoCollider().x += 150 * Gdx.graphics.getDeltaTime();
-			}
-		} else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			if(linko.getFlippedRight()) {
-				linko.setFlippedRight(false);
-			}
-			if(!linko.getJumping()){
-				batch.draw(linko.getRunningLeftAnimation().getKeyFrame(elapsed,true), linko.getLinkoCollider().x,linko.getLinkoCollider().y);
-			}
-			if(!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-				linko.getLinkoCollider().x -= 100 * Gdx.graphics.getDeltaTime();
-			} else {
-				linko.getLinkoCollider().x -= 150 * Gdx.graphics.getDeltaTime();
-			}
-		} 
 		
-		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && jumps < 2) {
-			linko.setJumping(true);
-			if(jumps == 0) {
-				linko.getLinkoCollider().y += (100 * Gdx.graphics.getDeltaTime()) + 75;
-				if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-					linko.getLinkoCollider().y += 50;
-				}
-				jump2 = linko.getLinkoCollider().y * 1.5f;
-				System.out.println("jump 1: " + linko.getLinkoCollider().y);
-			} else {
-				System.out.println("jump 2 start: " + linko.getLinkoCollider().y);
-				linko.getLinkoCollider().y = jump2;
-				System.out.println("jump 2: " + linko.getLinkoCollider().y);
-			}
-			jumps++;
-		} 
+		linko.move(batch);
+		linko2.move(batch);
+		linko3.move(batch);
 		
-		if(linko.getJumping()) {
-			jumpRender();
+		if(linko.getLinkoCollider().overlaps(linko2.getLinkoCollider())) {
+			linko2.setLinkoCollider(500,10);
+			if(linko.getHealth() > 0) {
+				linko.setHealth(linko.getHealth()-1);
+			}
+		}
+		if(linko.getLinkoCollider().overlaps(linko3.getLinkoCollider())) {
+			linko3.setLinkoCollider(500, 100);
 		}
 		
-		if(linko.getLinkoCollider().y > 11) {
-			linko.getLinkoCollider().y -= 120 * Gdx.graphics.getDeltaTime();
-		} else if (linko.getLinkoCollider().y < 11) {
-			jumps = 0;
-			linko.setJumping(false);
-		}
-		
-		batch.draw(otherLinko.getRunningLeftAnimation().getKeyFrame(elapsed2,true), other.x, other.y);
-		other.x -= 100 * Gdx.graphics.getDeltaTime();
-		
-		if(linko.getLinkoCollider().overlaps(other)) {
-			other.x = 500;
-			other.y = 10;
-		}
+		linko.healthRender(batch);
 		
 		batch.end();
 		camera.update();
-	}
-
-	private void jumpRender() {
-		if(linko.getFlippedRight()) {
-			batch.draw(linko.getJumpRight(), linko.getLinkoCollider().x, linko.getLinkoCollider().y);
-		} else {
-			batch.draw(linko.getJumpLeft(), linko.getLinkoCollider().x, linko.getLinkoCollider().y);
-		}
 	}
 
 	@Override
